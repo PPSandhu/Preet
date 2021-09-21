dbc2231a6de3eebed27654fede89847ddb8597ec
PPSandhu <91058762+PPSandhu@users.noreply.github.com>
2021-09-21 18:44:43 +1000

    changes

--- src/test/java/StepDefinations/SD01_NavigateToLoginScreen.java
+++ src/test/java/StepDefinations/SD01_NavigateToLoginScreen.java
@@ -13,12 +13,14 @@ public class SD01_NavigateToLoginScreen {
 	public void open_browser_and_navigate_to_reg_screen() throws IOException {
 		AT01_CreateAccount.AccCre();
 		
+		//test
 	}
 
 	@Then("I want to assert that i am on that screen")
 	public void i_want_to_assert_that_i_am_on_that_screen() {
 		AT01_CreateAccount.AssertPage();
 		
+		//test
 	}
 
 
